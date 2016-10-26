fd1090636dd64cf136a763ffc0d4c2d57f048aa5
Ahmed El Moden <ahmed.elmoden@imerir.com>
2016-10-26 17:34:34 +0200

    First commit

---
+++ src/main.cpp
@@ -0,0 +1,36 @@
+#include "Core.hpp"
+#include <sys/resource.h>
+
+#define PORT_ROBOT_MOTOR 5555
+#define DEFAULT_HOST_ADDRESS "10.0.1.1"
+
+
+int main( int argc, char** argv )
+{
+	std::string hostAdress = DEFAULT_HOST_ADDRESS;
+
+	int hostPort = PORT_ROBOT_MOTOR;
+
+	// core initialisation
+	Core* core = new Core();
+
+	if( argc > 1 )
+	{
+		hostAdress = argv[1];
+	}
+
+	if( argc > 2 )
+	{
+		hostPort = atoi( argv[2] );
+        }
+
+	// start main core thread
+	core->init( hostAdress, static_cast<uint16_t>( hostPort ) );
+
+	// waits the thread exits
+	core->joinMainThread();
+
+	delete core;
+
+	return 0;
+}
